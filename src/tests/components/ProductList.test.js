2d06b7fdbc855d4028a609397ba51f45cfe3530d
antonio <bowie.villasenor@yahoo.com>
2023-05-25 17:17:54 +0200

    testing git

--- src/tests/components/ProductList.test.js
+++ src/tests/components/ProductList.test.js
@@ -1,19 +1,31 @@
 import { render, screen } from "@testing-library/react";
-import {PRODUCT_MOCK_DATA} from '../mockdata/ProductMockData'
-import ProductList from '../../components/ProductList';
-import { GENERAL_LABELS } from '../../translations/english'
+import { PRODUCT_MOCK_DATA } from "../mockdata/ProductMockData";
+import ProductList from "../../components/ProductList";
+import { GENERAL_LABELS } from "../../translations/english";
 
+/**
+ * just testing the 2 behaviour for 'now'
+ */
 describe("ProductList", () => {
-  
   it("display product cards when product data are existing", () => {
-    render(<ProductList products={PRODUCT_MOCK_DATA.LIST_VALID} itemsPerRow={4}></ProductList>);
+    render(
+      <ProductList
+        products={PRODUCT_MOCK_DATA.LIST_VALID}
+        itemsPerRow={4}
+      ></ProductList>
+    );
     const iphone8Label = screen.getByText("iPhone 8");
     expect(iphone8Label).toBeInTheDocument();
   });
 
   it("display label no products available when the product list is empty", () => {
-    render(<ProductList products={PRODUCT_MOCK_DATA.LIST_EMPTY} itemsPerRow={4}></ProductList>);
-    const nodata = screen.getByText(GENERAL_LABELS.NO_AVAILABLE_DATA)
+    render(
+      <ProductList
+        products={PRODUCT_MOCK_DATA.LIST_EMPTY}
+        itemsPerRow={4}
+      ></ProductList>
+    );
+    const nodata = screen.getByText(GENERAL_LABELS.NO_AVAILABLE_DATA);
     expect(nodata).toBeInTheDocument();
   });
 });
